1d106a1bafa6670c623225e49033ecd1fc948182
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 19:37:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badges = []
+  index = 0 
+  while index < attendees.length 
+  badges << yield(attendees[index])
+  index += 1 
+ end
+   badges
+ end 
