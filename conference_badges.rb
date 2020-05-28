16cb5746cc76db64e2010cdd7ef637be1f1d2a07
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 19:44:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    badge_maker(attendee)
+  end 
+ end 
+
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end 
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  assign_rooms(attendees).each do |assigned_room|
+    puts assigned_room
+  end 
+end 
+    
+    
+    
+    
